4e6abc130a2cc45bb407e1feff0808595c008c26
BondVitalii <122458459+BondVitalii@users.noreply.github.com>
2023-05-15 18:50:04 +0300

    task 9

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,4 +1,21 @@
-//
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  const userInform = { email: email.value, Password: password.value };
+  event.currentTarget.reset();
+}
+
+// ----------
 
 /** Задание 8
 Задание 8
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('button.change-color');
+const body = document.querySelector('body');
+const colorValue = document.querySelector('span.color');
+
+button.addEventListener('click', elem => {
+  let color = getRandomHexColor();
+  body.style.background = color;
+  colorValue.textContent = color;
+});
+// ----------
+
+/** Задание 9
+Задание 9
+Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
+
+<div class="widget">
+  <p>Background color: <span class="color">-</span></p>
+  <button type="button" class="change-color">Change color</button>
+</div>
+
+Для генерации случайного цвета используй функцию getRandomHexColor.
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+*/
