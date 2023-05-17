9e68eab594ad9f8faea7b73043b6b34652cf4d32
BondVitalii <122458459+BondVitalii@users.noreply.github.com>
2023-05-17 13:04:56 +0300

    1

--- js/task-02.js
+++ js/task-02.js
@@ -66,3 +66,17 @@ const ingredients = [
 Добавит элементу класс item.
 После чего вставит все <li> за одну операцию в список ul.ingredients.
 */
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
